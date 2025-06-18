eff182a053326088fe4c62fef6f71c329c5aa7c3
Zamin786 <aqsaxammin786@gmail.com>
2025-06-18 18:32:44 +0530

    Safe checkpoint before fixing gh-pages

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+  // const earth = useGLTF("./planet/scene.gltf");
+  const earth = useGLTF(process.env.PUBLIC_URL + "/planet/scene.gltf");
+
   return (
     <primitive object={earth.scene} scale={3} position-y={0} rotation-y={0} />
   );
